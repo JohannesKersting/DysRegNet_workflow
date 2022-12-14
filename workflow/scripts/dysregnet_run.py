69b116058eace6b818ede87bc0df82eed47dc7c0
JohannesKersting <kerstingjohannes@gmail.com>
2022-12-14 17:04:42 +0100

    testing feather output

--- workflow/scripts/dysregnet_run.py
+++ workflow/scripts/dysregnet_run.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import dysregnet
 import argparse
+import pyarrow
 import sys
 
 
@@ -10,6 +11,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--expr', type=str, required=True)
 parser.add_argument('--meta', type=str, required=True)
 parser.add_argument('--grn', type=str, required=True)
+parser.add_argument('--output', type=str, required=True)
 
 args = parser.parse_args()
 
@@ -19,5 +21,7 @@ print("Arguments:")
 print(args)
 print("\n")
 
-print("test")
+df = pd.DataFrame({'Temperature': ['Hot', 'Cold', 'Warm', 'Cold'],
+                   })
+df.to_feather(args.output)
 
