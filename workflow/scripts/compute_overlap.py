55d648d581d83254c0232a111e1498adf188d7f6
JohannesKersting <kerstingjohannes@gmail.com>
2023-04-14 15:52:37 +0200

    support for signed overlaps

--- workflow/scripts/compute_overlap.py
+++ workflow/scripts/compute_overlap.py
@@ -13,56 +13,7 @@ import sys
 import argparse
 
 
-def get_overlap(x, y):
-    return (len(x.intersection(y)) / min(len(x), len(y)))
-
-
-def overlap_mat(patient_list, networks):
-    overlap_mat = np.zeros((len(patient_list), len(patient_list)))
-
-    idx = [(i, j) for i in range(len(patient_list)) for j in range(i, len(patient_list))]
-    for idx in tqdm(idx):
-        i, j = idx
-        set_1 = networks[i]
-        set_2 = networks[j]
-        try:
-            overlap = get_overlap(set(set_1), set(set_2))
-        except ZeroDivisionError:
-            overlap = 0
-
-        overlap_mat[i, j] = overlap
-        overlap_mat[j, i] = overlap_mat[i, j]
-
-    return pd.DataFrame(overlap_mat, index=patient_list, columns=patient_list)
-
-
-def main():
-    parser = argparse.ArgumentParser()
-
-    parser.add_argument('--input', nargs='+', required=True,
-                        help='Paths to multiple patient specific networks')
-    parser.add_argument("--shared", action="store_true",
-                        help="Consider only edges which appear in all input networks")
-    parser.add_argument('--output_edges', type=str, required=True,
-                        help='Path to the edge output file (will be stored in feather format)')
-    parser.add_argument('--output_nodes', type=str, required=True,
-                        help='Path to the node output file (will be stored in feather format)')
-
-    args = parser.parse_args()
-
-    print("Python version:")
-    print(sys.version)
-    print("Arguments:")
-    print(args)
-    print("\n")
-
-    input_list = args.input
-    output_edges = args.output_edges
-    output_nodes = args.output_nodes
-    only_shared_edges = args.shared
-
-    index_col = "patient id"
-
+def get_sets(input_list, index_col, only_shared_edges, sign):
     # list of node sets (one set for every patient)
     nodes = []
 
@@ -112,7 +63,15 @@ def main():
 
             # loop over edges
             for v1, v2 in all_edges:
-                if abs(crc[i, count]) > 0:
+                if sign == 0 and abs(crc[i, count]) > 0:
+                    node_set.add(v1)
+                    node_set.add(v2)
+                    edge_list.append((v1, v2))
+                elif sign > 0 and crc[i, count] > 0:
+                    node_set.add(v1)
+                    node_set.add(v2)
+                    edge_list.append((v1, v2))
+                elif sign < 0 and crc[i, count] < 0:
                     node_set.add(v1)
                     node_set.add(v2)
                     edge_list.append((v1, v2))
@@ -154,15 +113,123 @@ def main():
         print(f"Filtered number of dysregulated nodes in all networks and patients:", end=' ')
         print(sum([len(patient_nodes) for patient_nodes in nodes]))
 
-    overlap_edges = overlap_mat(patient_list, edges).reset_index(names=index_col)
-    print("Edge overlaps:")
-    print(overlap_edges)
-    overlap_edges.to_feather(output_edges)
+    return patient_list, edges, nodes
 
-    overlap_nodes = overlap_mat(patient_list, nodes).reset_index(names=index_col)
-    print("Node overlaps:")
-    print(overlap_nodes)
-    overlap_nodes.to_feather(output_nodes)
+def get_overlap(x, y):
+    return (len(x.intersection(y)) / min(len(x), len(y)))
+
+
+def overlap_mat(patient_list, networks):
+    overlap_mat = np.zeros((len(patient_list), len(patient_list)))
+
+    idx = [(i, j) for i in range(len(patient_list)) for j in range(i, len(patient_list))]
+    for idx in tqdm(idx):
+        i, j = idx
+        set_1 = networks[i]
+        set_2 = networks[j]
+        try:
+            overlap = get_overlap(set(set_1), set(set_2))
+        except ZeroDivisionError:
+            overlap = 0
+
+        overlap_mat[i, j] = overlap
+        overlap_mat[j, i] = overlap_mat[i, j]
+
+    return pd.DataFrame(overlap_mat, index=patient_list, columns=patient_list)
+
+def combined_overlaps(patient_list, index_col, pos, neg):
+    overlap_pos = overlap_mat(patient_list, pos)
+    overlap_neg = overlap_mat(patient_list, neg)
+    overlap = (overlap_pos + overlap_neg) / 2
+    overlap.reset_index(names=index_col)
+    return overlap
+
+
+def main():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--input', nargs='+', required=True,
+                        help='Paths to multiple patient specific networks')
+    parser.add_argument("--shared", action="store_true",
+                        help="Consider only edges which appear in all input networks")
+    parser.add_argument("--signed", action="store_true",
+                        help="The overlap calculation should distinguish between positive and negative values in the input network")
+    parser.add_argument('--output_edges', type=str, required=True,
+                        help='Path to the edge output file (will be stored in feather format)')
+    parser.add_argument('--output_nodes', type=str, required=True,
+                        help='Path to the node output file (will be stored in feather format)')
+
+    args = parser.parse_args()
+
+    print("Python version:")
+    print(sys.version)
+    print("Arguments:")
+    print(args)
+    print("\n")
+
+    input_list = args.input
+    output_edges = args.output_edges
+    output_nodes = args.output_nodes
+    only_shared_edges = args.shared
+    signed = args.signed
+
+    index_col = "patient id"
+
+    if not signed:
+
+        patient_list, edges, nodes = get_sets(input_list, index_col=index_col, only_shared_edges=only_shared_edges, sign=0)
+
+        overlap_edges = overlap_mat(patient_list, edges).reset_index(names=index_col)
+        print("Edge overlaps:")
+        print(overlap_edges)
+        overlap_edges.to_feather(output_edges)
+        overlap_edges = None
+        edges = None
+
+        overlap_nodes = overlap_mat(patient_list, nodes).reset_index(names=index_col)
+        print("Node overlaps:")
+        print(overlap_nodes)
+        overlap_nodes.to_feather(output_nodes)
+
+        overlap_nodes = None
+        nodes = None
+
+    else:
+        print("\nReading positive values:")
+        patient_list_pos, edges_pos, nodes_pos = get_sets(input_list, index_col=index_col, only_shared_edges=only_shared_edges, sign=1)
+
+        print("\nReading negative values:")
+        patient_list_neg, edges_neg, nodes_neg = get_sets(input_list, index_col=index_col, only_shared_edges=only_shared_edges, sign=-1)
+
+        assert patient_list_pos == patient_list_neg
+
+        overlap_edges = combined_overlaps(patient_list_pos, index_col, edges_pos, edges_neg)
+        edges_pos = 0
+        edges_neg = 0
+
+        print("Edge overlaps:")
+        print(overlap_edges)
+        overlap_edges.to_feather(output_edges)
+        overlap_edges = None
+
+
+        overlap_nodes = combined_overlaps(patient_list_pos, index_col, nodes_pos, nodes_neg)
+        nodes_pos = None
+        nodes_neg = None
+
+        print("Node overlaps:")
+        print(overlap_nodes)
+        overlap_nodes.to_feather(output_nodes)
+        overlap_nodes= None
+
+
+
+
+
+
+
+
+
 
 
 if __name__ == "__main__":
