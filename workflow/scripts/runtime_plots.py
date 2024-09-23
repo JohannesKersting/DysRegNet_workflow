35725fe94e8cca6d8126a3cc70b002bbfef1e7da
JohannesKersting <kerstingjohannes@gmail.com>
2024-09-23 09:35:20 +0200

    added runtime in minutes

--- workflow/scripts/runtime_plots.py
+++ workflow/scripts/runtime_plots.py
@@ -10,7 +10,9 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--input',  nargs='+', required=True,
                         help='Snakemake benchmark files, tile name will be used to identify the method.')
-    parser.add_argument('--output',  type=str, required=True,
+    parser.add_argument('--output_sec',  type=str, required=True,
+                        help='Output path to the result .png (will also be saved as .pdf)')
+    parser.add_argument('--output_min',  type=str, required=True,
                         help='Output path to the result .png (will also be saved as .pdf)')
 
     args = parser.parse_args()
@@ -19,7 +21,8 @@ def main():
     print(f"Arguments:\n{args}\n")
 
     inputs = args.input
-    output_path = args.output
+    output_sec_path = args.output_sec
+    output_min_path = args.output_min
 
 
     # Read data
@@ -37,13 +40,14 @@ def main():
     df = pd.concat(df_list, ignore_index=True)
     # map method name
     df["method"] = df["method"].map(method_lut)
+    df["minutes"] = df["s"] / 60
     print(df)
 
     print("\nMean run-time [s]:")
     print(df.groupby("method")["s"].mean().to_string())
 
 
-    # Plot
+    # Plot in secods
     sns.set(style="whitegrid")
 
     sns.boxplot(data=df, x="method", y="s")
@@ -51,8 +55,21 @@ def main():
     plt.xlabel("Method")
     plt.tight_layout()
 
-    plt.savefig(output_path, dpi=300)
-    plt.savefig(os.path.splitext(output_path)[0] + '.pdf', dpi=300)
+    plt.savefig(output_sec_path, dpi=300)
+    plt.savefig(os.path.splitext(output_sec_path)[0] + '.pdf', dpi=300)
+    plt.clf()
+
+    # Plot in minutes
+    sns.set(style="whitegrid")
+
+    sns.boxplot(data=df, x="method", y="minutes")
+    plt.ylabel("Time [m]")
+    plt.xlabel("Method")
+    plt.tight_layout()
+
+    plt.savefig(output_min_path, dpi=300)
+    plt.savefig(os.path.splitext(output_min_path)[0] + '.pdf', dpi=300)
+    plt.clf()
 
 if __name__ == "__main__":
     main()
